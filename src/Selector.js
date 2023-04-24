0c7385d5c8633317b2d04525879beee33552814f
Nathan Fiscaletti <nate.fiscaletti@gmail.com>
2023-04-24 12:52:55 -0500

    Updated selector with font-awesome icons

--- src/Selector.js
+++ src/Selector.js
@@ -1,4 +1,6 @@
-const React = require('react');
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFloppyDisk, faXmark } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
 
 const { ipcRenderer } = window.require('electron');
 
@@ -20,34 +22,36 @@ export default function Selector() {
             alignItems: "center",
         }}>
             <h3 style={{ color: 'white', textAlign: 'center', marginBottom: '16px' }}>Resize / Position Window to Select Region</h3>
-            <div 
-                style={{ 
-                    WebkitAppRegion: 'no-drag',
-                    padding: '8px',
-                    border: '1px solid green',
-                    backgroundColor: saveHover ? 'green' : 'black',
-                    color: 'white',
-                    marginBottom: '16px'
-                }}
-                onMouseEnter={() => setSaveHover(true)}
-                onMouseLeave={() => setSaveHover(false)}
-                onClick={() => ipcRenderer.send('saveRegion')}
-            >
-                Save Selection
-            </div>
-            <div
-                style={{
-                    WebkitAppRegion: 'no-drag',
-                    padding: '8px',
-                    border: '1px solid red',
-                    backgroundColor: cancelHover ? 'red' : 'black',
-                    color: 'white'
-                }}
-                onMouseEnter={() => setCancelHover(true)}
-                onMouseLeave={() => setCancelHover(false)}
-                onClick={() => ipcRenderer.send('closeRegionSelector')}
-            >
-                Cancel
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                <div
+                    style={{
+                        WebkitAppRegion: 'no-drag',
+                        padding: '8px',
+                        border: '1px solid red',
+                        backgroundColor: cancelHover ? 'red' : 'black',
+                        color: 'white',
+                        marginRight: '16px'
+                    }}
+                    onMouseEnter={() => setCancelHover(true)}
+                    onMouseLeave={() => setCancelHover(false)}
+                    onClick={() => ipcRenderer.send('closeRegionSelector')}
+                >
+                    <FontAwesomeIcon icon={faXmark} /> Cancel
+                </div>
+                <div 
+                    style={{ 
+                        WebkitAppRegion: 'no-drag',
+                        padding: '8px',
+                        border: '1px solid green',
+                        backgroundColor: saveHover ? 'green' : 'black',
+                        color: 'white'
+                    }}
+                    onMouseEnter={() => setSaveHover(true)}
+                    onMouseLeave={() => setSaveHover(false)}
+                    onClick={() => ipcRenderer.send('saveRegion')}
+                >
+                    <FontAwesomeIcon icon={faFloppyDisk} /> Save Selection
+                </div>
             </div>
         </div>
     );
